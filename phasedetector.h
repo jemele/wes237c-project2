758f5ecda9f681bdcae01010643856112c244990
Joshua Emele <jemele@acm.org>
2015-11-01 22:37:25 -0800

    Build

--- phasedetector.h
+++ phasedetector.h
@@ -6,35 +6,17 @@
 */
 #ifndef PHASE_DETECTOR_H_
 #define PHASE_DETECTOR_H_
-//#define pi2  data_t(1.57079633)
 
 #include <ap_fixed.h>
 const int N=32;
 #define NO_ITER 16
 typedef ap_fixed<64,8> fix_num;
-
 typedef float data_t;
 
-void phasedetector (
-  data_t I,
-  data_t Q,
-
-  data_t *R,
-  data_t *theta
-  );
-
-void cordiccart2pol(data_t x, data_t y, data_t * r,  data_t * theta);
-
-void fir (
-  data_t I,
-  data_t Q,
-
-  data_t *X,
-  data_t *Y
-  );
-
+void fir(float I, float Q, float *X, float *Y); 
+void phasedetector(float *input, float *output);
+void cordiccart2pol(float x, float y, float *r, float *theta);
 void correct_quad(fix_num * x, fix_num * y, int * quad);
-
 void restore_quad(fix_num *theta, int quad);
 
 #endif
