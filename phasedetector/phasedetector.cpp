f5c43131def9c89955bce91241ada40de60667e1
Joshua Emele <jemele@acm.org>
2015-11-02 20:16:53 -0800

    pipeline and unroll

--- phasedetector/phasedetector.cpp
+++ phasedetector/phasedetector.cpp
@@ -19,6 +19,7 @@ void phasedetector(float *in, float *out)
 #pragma AP interface ap_fifo port=in
 #pragma AP interface ap_fifo port=out
 #pragma AP interface ap_ctrl_none port=return
+#pragma HLS PIPELINE II=3
 
     // Read input from host.
     fix_num i = *in++;
